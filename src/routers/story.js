8dd99f63b51fb276cd1e545efa10d9f2ca778d0e
shortcrimson <toby.jones@protonmail.com>
2019-12-27 16:48:13 +0000

    Story CRUD Completed

--- src/routers/story.js
+++ src/routers/story.js
@@ -13,7 +13,7 @@ router.post('/stories', async (req, res) => {
 	}
 });
 
-//Get
+//Read
 router.get('/stories', async (req, res) => {
 	try {
 		const stories = await Story.find(req.query);
@@ -22,5 +22,51 @@ router.get('/stories', async (req, res) => {
 		res.status(500).send(e);
 	}
 });
+
+router.get('/stories/:id', async (req, res) => {
+	try {
+		const story = await Story.findById(req.params.id);
+		if (!story) {
+			return res.status(404).send();
+		}
+		res.send(story);
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
+//Update
+router.patch('/stories/:id', async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['title', 'description', 'priority'];
+	const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates' });
+	}
+	try {
+		const story = await Story.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+		if (!story) {
+			return res.status(404).send();
+		}
+		res.send(story);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+
+//Delete
+router.delete('/stories/:id', async (req, res) => {
+	try {
+		const story = await Story.findByIdAndDelete(req.params.id);
+		if (!story) {
+			return res.status(404).send();
+		}
+		res.send(story);
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
+
 
 module.exports = router;
